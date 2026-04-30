64d899fd496f8a9bdd5ed3c1b4209a38603850bd
BABALOLA IDRIS TEMITOPE <101128171+HedrisTemmyTop@users.noreply.github.com>
2026-04-30 12:28:40 +0100

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        let map = new Map()
+        let visited = new Set()
+        let count =0;
+
+        for(let node =0; node < n; node++){
+            map.set(node, [])
+        }
+        for(let [a, b] of edges){
+            map.set(a,[...map.get(a), b])
+            map.set(b,[...map.get(b), a])
+        }
+
+        function dfs(node){
+            if(visited.has(node)) return
+            visited.add(node)
+            for(let nei of map.get(node)){
+                dfs(nei)
+            }
+            return;
+        }
+
+        for(let node =0; node < n; node++){
+            if(visited.has(node)) continue
+            dfs(node)
+            count++
+        }
+
+        return count;
+    }
+}
